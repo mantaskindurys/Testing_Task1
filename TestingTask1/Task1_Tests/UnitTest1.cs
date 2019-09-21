57a07434dc132a5b0b4afd4da17b6e4892153c45
Mantas Kindurys <mantaskindurys@users.noreply.github.com>
2019-09-21 15:04:02 +0300

    Circle and Rectangle tests

--- TestingTask1/Task1_Tests/UnitTest1.cs
+++ TestingTask1/Task1_Tests/UnitTest1.cs
@@ -17,7 +17,7 @@ namespace Task1_Tests
 
             Square Square = new Square();
 
-            double actual = Square.getPerimeter(sideLenght);
+            double actual = Square.GetPerimeter(sideLenght);
 
             Assert.AreEqual(expectedResult, actual, 0);
             
@@ -26,7 +26,69 @@ namespace Task1_Tests
         [TestMethod]
         public void TestMethodGetArea()
         {
+            double sideLenght = 4;
+            double expectedResult;
+
+            expectedResult = sideLenght * sideLenght;
+
+            Square Square = new Square();
+
+            double actual = Square.GetArea(sideLenght);
+
+            Assert.AreEqual(expectedResult, actual, 0);
         }
 
     }
+    [TestClass]
+    public class RectangleTest
+    {
+        [TestMethod]
+        public void TestMethodGetPerimeter()
+        {
+            Rectangle Rectangle = new Rectange();
+            double Lenght = 4;
+            double Height = 4;
+            expectedResult = (Lenght * x) + (Height * 2);
+            double actual = Rectangle.GetPerimeter(Lenght, Height);
+            Assert.AreEqual(expectedResult, actual, 0);
+        }
+
+        [TestMethod]
+        public void TestMethodGetArea()
+        {
+            Rectangle Rectangle = new Rectange();
+            double Lenght = 4;
+            double Height = 4;
+            expectedResult = (Lenght * Height);
+            double actual = Rectangle.GetArea(Lenght, Height);
+            Assert.AreEqual(expectedResult, actual, 0);
+        }
+    }
+
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void TestMethodGetCircumference()
+        {
+            Circle Circle = new Circle();
+            double r = 4;
+            double pi = 3.14;
+            expectedResult = (2 * pi * r);
+            double actual = Circle.GetCircumference(r);
+            Assert.AreEqual(expectedResult, actual, 0);
+        }
+
+        [TestMethod]
+        public void TestMethodGetArea()
+        {
+            Circle Circle = new Circle();
+            double r = 4;
+            double pi = 3.14;
+            expectedResult = (pi * r * r);
+            double actual = Circle.GetArea(r);
+            Assert.AreEqual(expectedResult, actual, 0);
+        }
+    }
+
 }
