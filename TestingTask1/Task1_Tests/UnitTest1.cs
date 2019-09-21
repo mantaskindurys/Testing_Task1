12e673f1ae47e3c37a4591bd1a524c36fedfe100
Eimantas Ilkevicius <eimantas1998@gmail.com>
2019-09-21 14:44:55 +0300

    Added square test

--- TestingTask1/Task1_Tests/UnitTest1.cs
+++ TestingTask1/Task1_Tests/UnitTest1.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingTask1._2_Dimensional;
 
 namespace Task1_Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class SquareTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodGetPerimeter()
+        {
+            double sideLenght = 4;
+            double expectedResult;
+
+            expectedResult = sideLenght * 4;
+
+            Square Square = new Square();
+
+            double actual = Square.getPerimeter(sideLenght);
+
+            Assert.AreEqual(expectedResult, actual, 0);
+            
+        }
+
+        [TestMethod]
+        public void TestMethodGetArea()
         {
         }
+
     }
 }
---
+++ TestingTask1/TestingTask1/Two_Dimensional/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTask1._2_Dimensional
+{
+    public class Square
+    {
+        public static double getPerimeter(double sideLenght)
+        {
+            if(sideLenght <= 0)
+            {
+                throw new System.ArgumentException("Parameter cannot be less or equal to 0", "sideLenght");
+            }
+            else
+            {
+                double perimeter = sideLenght * 4;
+                return perimeter;
+            }
+            
+        }
+    }
+}
